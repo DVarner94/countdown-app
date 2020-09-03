29a7bffd8eacd55dc614b2b2770ff7a1861554c6
Dominic Varner <dommy@MLBs-Lappy.local>
2020-09-03 10:24:45 -0700

    update countdown function, calc remain time

--- countdown-app.js
+++ countdown-app.js
@@ -6,4 +6,16 @@ const countdown = document.getElementById('countdown');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`)
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown () {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    // added % so that we get the given const's time remaining
+}
+
